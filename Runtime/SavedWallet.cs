3e1e431ae7b6ef3133788bb8e7e83d88ff0c24c1
mdlka <mogilevskiy499@gmail.com>
2022-10-20 16:39:08 +0300

    Add decorator SavedWallet. Refactor benchmark

---
+++ Runtime/SavedWallet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Agava.WalletTemplate
+{
+    public sealed class SavedWallet<T> : IWallet<T> where T : IComparable, IComparable<T>
+    {
+        private readonly IWallet<T> _wallet;
+        private readonly WalletSave<T> _save;
+
+        public SavedWallet(WalletSave<T> save)
+        {
+            _save = save;
+            _wallet = save.Load();
+        }
+
+        public T Value => _wallet.Value;
+        
+        public void Add(T amount)
+        {
+            _wallet.Add(amount);
+            _save.Save(_wallet);
+        }
+
+        public void Remove(T amount)
+        {
+            _wallet.Remove(amount);
+            _save.Save(_wallet);
+        }
+    }
+}
--- Runtime/Test.cs
+++ Runtime/Test.cs
@@ -1,20 +1,15 @@
-using System.Diagnostics;
-using Agava.WalletTemplate.MathOperations;
+using System.Numerics;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
-using Random = UnityEngine.Random;
 
 namespace Agava.WalletTemplate
 {
     public class Test : MonoBehaviour
     {
-        private WalletSave<int> _walletSave;
         private IWallet<int> _wallet;
         
         private void Start()
         {
-            _walletSave = new WalletSave<int>("123", new PlayerPrefsJsonSaveLoad());
-            _wallet = _walletSave.Load();
+            _wallet = new SavedWallet<int>(new WalletSave<int>("123"));
         }
 
         [ContextMenu("Add")]
@@ -29,40 +24,6 @@ namespace Agava.WalletTemplate
         {
             _wallet.Remove(1);
             Debug.Log(_wallet.Value);
-        }
-
-        [ContextMenu("Save")]
-        private void Save()
-        {
-            _walletSave.Save(_wallet);
-        }
-        
-        [ContextMenu("Time")]
-        private void StartTime()
-        {
-            var watch = new Stopwatch();
-            int sum1 = 0;
-
-            int n = Random.Range(99999, 100002);
-            
-            watch.Start();
-            for (int i = 0; i < n; i++)
-                sum1 = i + i;
-            watch.Stop();
-            
-            Debug.Log($"Default operation: {watch.Elapsed.TotalMilliseconds} milliseconds; n: {n}, {sum1}");
-
-            int sum2 = 0;
-            watch.Reset();
-            
-            var expression = new CachedExpressionLambda<int>(new AddExpressionLambda<int>());
-
-            watch.Start();
-            for (int i = 0; i < n; i++)
-                sum2 = expression.Compile()(i, i);
-            watch.Stop();
-
-            Debug.Log($"Expression operation: {watch.Elapsed.TotalMilliseconds} milliseconds; n: {n}, {sum2}");
         }
     }
 }
